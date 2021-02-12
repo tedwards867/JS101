d9438e3a009e6dfeb005591974f53f08d7d5b8cd
Tara Edwards <tara.edwards867@gmail.com>
2021-02-12 11:13:24 -0700

    eslint updates

--- Lesson_2/MortgageCalculator/loanCalculator.js
+++ Lesson_2/MortgageCalculator/loanCalculator.js
@@ -1,17 +1,4 @@
-//greet user
-//ask how much they want to borrow
-//ask how many years will the loan be
-//ask what is the APR
-//calculate the monthly interest rate (APR/loan duration months)
-//determine loan months (year/12
-//calculate monthly payment -> loanAmount * (monthlyIntRate / (1 - Math.pow((1 + monthlyIntRate), (-loanDuration))))
-//display the montly payment amount with $ and decimals.
-
-//limit loan years to 30
-//what are valid inputs for interest rate?
-//no-interest loans?
-//allow user to determine if duration is months or years?
-
+// loanCalculator.js
 const MESSAGES = require('./calcMessages.json');
 const readline = require('readline-sync');
 
@@ -23,39 +10,48 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number)) || number === '0';
 }
 
-prompt(MESSAGES['welcome']);
+prompt(MESSAGES.welcome);
 
 //add while loop for another calculation
 while (true) {
 
-  prompt(MESSAGES['loanAmountPrompt']);
+  prompt(MESSAGES.loanAmountPrompt);
   let loanAmount = readline.question();
 
   while (invalidNumber(loanAmount)) {
-    prompt(MESSAGES['invalidNumberMessage']);
+    prompt(MESSAGES.invalidNumberMessage);
     loanAmount = readline.question();
   }
 
-  prompt(MESSAGES['loanDurationYearsPrompt']);
+  prompt(MESSAGES.loanDurationYearsPrompt);
   let loanDurationYears = readline.question();
 
   while (invalidNumber(loanDurationYears)) {
-    prompt(MESSAGES['invalidNumberMessage']);
+    prompt(MESSAGES.invalidNumberMessage);
     loanDurationYears = readline.question();
   }
   prompt(MESSAGES['APRPrompt']);
   let APR = readline.question();
-  let loanDurationMonths = parseInt(loanDurationYears * 12); //what is someone enters 6.5?
-  //prompt(`Amount to Borrow: ${loanAmount}, Loan Duration in Years: ${loanDurationYears}, Loan Duration in Months: ${loanDurationMonths}, APR: ${APR} `)
+  let loanDurationMonths = parseInt(loanDurationYears * 12);
+
 
   let monthlyInterestRate = parseFloat(APR) / loanDurationMonths;
-  let monthlyPayment = parseFloat(loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationMonths))))).toFixed(2);
-
+  let monthlyPayment;
+  if (APR > 0) {
+    monthlyPayment = parseFloat(loanAmount * (monthlyInterestRate /
+       (1 - ((1 + monthlyInterestRate) ** (-loanDurationMonths))))).toFixed(2);
+  } else {
+    monthlyPayment = parseFloat(loanAmount / loanDurationMonths).toFixed;
+  }
   prompt(`Your monthly payment for ${loanDurationYears} years will be $${monthlyPayment}`);
 
   //end another calcuation loop
-  prompt(MESSAGES['anotherPrompt']);
+  prompt(MESSAGES.anotherPrompt);
   let anotherCalc = readline.question();
-  if (anotherCalc[0].toLowerCase()  !== 'y') break;
+  if (anotherCalc[0].toLowerCase()  !== 'y') {
+    break;
+  } else {
+    console.clear();
+  }
 
 }
